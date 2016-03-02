f9e48f02ef4b9b39ff1f93eaadce995208fed1d6
Maxime Caly <max.caly@gmail.com>
2016-03-02 20:47:52 +0100

    Added favorite stream front-end and back-end

--- js/popup.js
+++ js/popup.js
@@ -5,12 +5,67 @@ $(document).ready(function() {
     var broadcasterLanguage = "fr"
     var notifiedWhenOnline;
 
+    //localstroage array
+    var notifiedList = JSON.parse(localStorage.getItem("favStreams"));
 
+    var checkStreamsAjax = function(link) {
+        $.ajax({
+            url: link,
+            dataType: 'json',
+            success: function(res) {
+                console.log(res)
+                for (stream of res.streams) {
+                    console.log(notifiedList);
+                    chrome.notifications.create(
+                        'Notification-test', {
+                            type: 'basic',
+                            iconUrl: 'img/icon.png',
+                            title: stream.channel.display_name + "is online",
+                            message: "Click Me if you want to see the stream!"
+                        },
+                        function() {}
+
+                    );
+                }
+            },
+            error: function(res) {},
+            complete: function(res) {},
+        });
+    };
 
     var openStreamLink = function(notificationId) {
         window.open(stream.channel.url, '_blank');
     }
 
+    var isStreamOnline = function(notifiedList) {
+        var channels = "";
+        for (object of notifiedList) {
+            channels += object['stream-name'] + ",";
+        }
+        var link = "https://api.twitch.tv/kraken/streams?stream_type=live&channel=" + channels;
+
+        //Remove last comma
+        link = link.slice(0, -1);
+        checkStreamsAjax(link);
+    }
+
+    $('#stream-results').on('click', '.fa.fa-star', function(event) {
+        var data = $(this).data("stream-name");
+        var obj = {};
+        if ($(this).hasClass('active-star')) {
+            $(this).removeClass('active-star');
+            notifiedList = notifiedList.filter(function(obj) {
+                return obj['stream-name'] != data;
+            });
+        } else {
+            $(this).addClass('active-star');
+            obj['stream-name'] = data;
+            obj['isNotified'] = false;
+            notifiedList.push(obj);
+            isStreamOnline(notifiedList);
+        }
+        localStorage.setItem("favStreams", JSON.stringify(notifiedList));
+    });
 
     $("#checkbox-get-notified").change(function(event) {
         if ($(this).prop("checked") === true) {
@@ -28,7 +83,7 @@ $(document).ready(function() {
 
     var getStreamList = function(language) {
         $.ajax({
-            url: 'https://api.twitch.tv/kraken/search/streams?q=starcraft&limit=500',
+            url: 'https://api.twitch.tv/kraken/streams?game=StarCraft+II&limit=500',
             dataType: 'json',
             success: function(res) {
                 liveStreams = res.streams;
@@ -38,10 +93,14 @@ $(document).ready(function() {
                 }
                 console.log(liveBroadcasterLanguageStreams);
                 for (stream of liveBroadcasterLanguageStreams) {
-                    console.log(stream.channel.logo)
-                    $("#stream-results").append('<div class="stream"><a class="stream-title" target="_blank" href="' + stream.channel.url + '">' + stream.channel.display_name + '</a><span>' + stream.channel.status + '</span><span>Viewers: ' + stream.viewers + '</span> <div class="stream-logo" style="background-image:url(' + stream.channel.logo + ')"></div></div>');
+                    console.log(stream);
+                    $("#stream-results").append('<div class="stream"><a class="stream-title" target="_blank" href="' + stream.channel.url + '">' + stream.channel.display_name + '</a><span>' + stream.channel.status + '</span><span>Viewers: ' + stream.viewers + '</span> <div class="stream-logo" style="background-image:url(' + stream.channel.logo + ')"></div><i class="fa fa-star fa-lg" data-stream-name=' + stream.channel.display_name + '></i></div>');
                     //Race icon if race found in stream status
                     //stream.channel.status.match("[zZ]erg");
+                }
+                for (favoriteStream of notifiedList) {
+                    var elem = $("[data-stream-name*='" + favoriteStream['stream-name'] + "']");
+                    elem.addClass("active-star");
                 }
             },
             error: function(res) {
@@ -53,16 +112,8 @@ $(document).ready(function() {
             },
         });
     };
-    localStorage.setItem("broadcasterLanguage", broadcasterLanguage);
-    $('#select-broadcaster-language').change(function() {
-
-        liveBroadcasterLanguageStreams = [];
-        $("#stream-results").html("");
-        broadcasterLanguage = $(this).val();
-        getStreamList(broadcasterLanguage);
 
 
-    });
 
     $("#option-icon").click(function(event) {
         $("#option-popup").css({
@@ -80,20 +131,18 @@ $(document).ready(function() {
         });
     });
 
+
+    $('#select-broadcaster-language').change(function() {
+        liveBroadcasterLanguageStreams = [];
+        $("#stream-results").html("");
+        broadcasterLanguage = $(this).val();
+        getStreamList(broadcasterLanguage);
+    });
+
+    chrome.notifications.onClicked.addListener(openStreamLink);
+
+    localStorage.setItem("broadcasterLanguage", broadcasterLanguage);
+
+
     loadPreferences();
 });
-
-/* This code is for when the user adds favorite streams and will be notified when they go online
-chrome.notifications.onClicked.addListener(openStreamLink);
-chrome.notifications.create(
-    'Notification-test', {
-        type: 'basic',
-        iconUrl: 'icon.png',
-        title: stream.channel.display_name + "is online",
-        message: "Click Me if you want to see the stream!"
-    },
-
-    function() {}
-
-);
-*/
