96bfab9c59d6c0f6fb1201b925e5f7a203b242ae
Max <max.caly@gmail.com>
2017-01-16 22:22:53 -0700

    Add korean language filter

--- src/client/app/components/StreamSelector/streamselector.jsx
+++ src/client/app/components/StreamSelector/streamselector.jsx
@@ -7,6 +7,7 @@ const supportedLanguages = [
   { abbr: 'all', caption: "All" },
   { abbr: "en", caption: "English" },
   { abbr: "fr", caption: "French" },
+  { abbr: "ko", caption: "Korean" },
   { abbr: "pl", caption: "Polish" },
   { abbr: "de", caption: "German" },
   { abbr: "es", caption: "Spanish" },
@@ -25,7 +26,7 @@ class StreamSelector extends React.Component {
     return (
       <div>
         <select className={style.streamSelector} onChange={this.props.onChange}>
-          {supportedLanguages.map((el,index) => {
+          {supportedLanguages.map((el, index) => {
             return (
               <option key={index} value={el.abbr}>{el.caption}</option>
             )
