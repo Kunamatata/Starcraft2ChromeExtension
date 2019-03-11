65594c3b760481a8f04b3321ed74af2327595ecd
Maxime Caly <max.caly@gmail.com>
2019-03-11 01:25:34 -0700

    Change styling rules Clean streamlist component code

--- src/client/app/components/StreamList/streamlist.jsx
+++ src/client/app/components/StreamList/streamlist.jsx
@@ -11,27 +11,30 @@ class StreamList extends React.Component {
       streams: null,
       favoriteStreams: []
     }
+    this.abortController = new AbortController();
     this.handleFavorite = this.handleFavorite.bind(this)
   }
 
-  componentWillMount() {
-    this.getStreamList()
-    // this.getStreamDummyList()
-    // chrome.storage.sync.get('favoriteStreams', (items) => {
-    //   if (items.favoriteStreams === undefined)
-    //     items.favoriteStreams = [];
-    //   this.setState({
-    //     favoriteStreams: items.favoriteStreams
-    //   });
-    // })
-  }
-
   componentDidMount() {
+    // this.getStreamDummyList()
+    chrome.storage.sync.get('favoriteStreams', (items) => {
+      if (items.favoriteStreams === undefined)
+        items.favoriteStreams = [];
+      this.setState({
+        favoriteStreams: items.favoriteStreams
+      });
+    })
     this.startPoll();
   }
 
+  componentWillUnmount() {
+    this.abortController.abort();
+    clearTimeout(this.timer);
+  }
+
   startPoll() {
-    this.interval = setInterval(() => this.getStreamList(), 60000);
+    this.getStreamList();
+    this.timer = setTimeout(() => this.startPoll(), 6000)
   }
 
   getStreamDummyList() {
@@ -41,7 +44,7 @@ class StreamList extends React.Component {
   }
 
   getStreamList() {
-    fetch(this.props.STARCRAFT_API_URL).then((response) => {
+    fetch(this.props.STARCRAFT_API_URL, {signal: this.abortController.signal}).then((response) => {
       if (response.status !== 200) {
         console.log('Looks like there was a problem. Status Code: ' +
           response.status)
@@ -52,6 +55,8 @@ class StreamList extends React.Component {
           streams: data.streams
         });
       })
+    }).catch(e => {
+      console.log(e);
     })
   }
 
@@ -66,21 +71,25 @@ class StreamList extends React.Component {
     this.setState({
       favoriteStreams
     });
-    // chrome.storage.sync.set({ 'favoriteStreams': this.state.favoriteStreams }, () => {
-    //   console.log('saved');
-    // });
+    chrome.storage.sync.set({ 'favoriteStreams': this.state.favoriteStreams }, () => {
+      console.log('saved');
+    });
   }
 
   render() {
-    if (this.state.streams) {
-      let streams = this.state.streams.filter((stream) => {
+    let {streams = null } = this.state;
+    
+    if (streams !== null) {
+      streams = streams.filter((stream) => {
         return this.props.language === 'all' ? true : stream.channel.broadcaster_language === this.props.language
       })
       
       return (
         <div className={style.streamList}>
           {streams.length > 0 ? streams.map((stream) => {
-            return (<Stream key={stream._id} stream={stream} favorite={this.state.favoriteStreams.indexOf(stream.channel.display_name) > -1 ? true : false} handleFavorite={this.handleFavorite} />)
+            return (
+            <Stream key={stream._id} stream={stream} favorite={this.state.favoriteStreams.indexOf(stream.channel.display_name) > -1 ? true : false} handleFavorite={this.handleFavorite} />
+            )
           }) : <div className={style.noStreams}>Currently no live streams for this broadcast language</div>}
         </div>
       )
