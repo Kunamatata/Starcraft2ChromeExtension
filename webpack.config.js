bc34c92f2803ff8e37a39f02c3be5d5cf800a4c6
Maxime Caly <max.caly@gmail.com>
2019-03-10 23:04:18 -0700

    Update dependencies and build and packaging

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,61 @@
 var webpack = require('webpack')
 var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var CopyWebPackPlugin = require('copy-webpack-plugin')
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public')
+var BUILD_DIR = path.resolve(__dirname, 'dist')
+console.log(BUILD_DIR)
 var APP_DIR = path.resolve(__dirname, 'src/client/app')
+console.log(APP_DIR)
 
 var config = {
   entry: {
-    javascript: APP_DIR + '/app.jsx',
-    background: APP_DIR + '/eventPage.js',
-    html: APP_DIR + '/index.html'
+    app: APP_DIR + '/app.jsx',
+    background: APP_DIR + '/eventPage.js'
   },
   output: {
     path: BUILD_DIR,
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   resolve: {
-    extensions: ['', '.js', '.jsx', '.json']
+    extensions: ['.js', '.jsx', '.json']
   },
   module: {
-    loaders: [
-      {
+    rules: [{
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: ['react-hot', 'babel-loader']
-      },
-      { 
-        test: /eventPage.js$/, 
-        loader: 'file?name=[name].[ext]' 
-      },
-      { 
-        test: /manifest.json$/, 
-        loader: 'file?name=[name].[ext]' 
+        use: ['babel-loader']
       },
       {
-        test: /\.html$/,
-        exclude: /node_modules/,
-        loader: 'file?name=[name].[ext]'
+        test: /eventPage.js$/,
+        loader: 'file-loader?name=[name].[ext]'
       },
+      // {
+      //   test: /\.html$/,
+      //   exclude: /node_modules/,
+      //   loader: 'html-loader?name=[name].[ext]'
+      // },
       {
         test: /\.styl$/,
         exclude: /node_modules/,
         loader: 'style-loader!css-loader?sourceMap&modules&camelCase!stylus-loader'
       },
-      { 
-        test: /\.png$/, 
-        loader: 'file?name=assets/[name].[ext]' 
+      {
+        test: /\.png$/,
+        loader: 'file-loader?name=assets/[name].[ext]'
       }
     ]
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+      inject: 'body',
+      template: APP_DIR + '/index.html'
+    }),
+    new CopyWebPackPlugin([{ from: APP_DIR + '/manifest.json' }]),
+    // new BundleAnalyzerPlugin({
+    //     analyzerPort: 8889,
+    // }),
+  ]
 }
 
 module.exports = config
